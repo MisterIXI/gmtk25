50fd1821813dfd203881e563a7e720ecb974afb9
Robert Moegenburg <r.moegenburg@t-online.de>
2025-07-31 11:03:04 +0200

    robo_chan

--- scenes/Entities/Player/robo_chan.gd
+++ scenes/Entities/Player/robo_chan.gd
@@ -1,9 +1,12 @@
 extends CharacterBody3D
 
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
-
+const SPEED : float = 7.5
+const JUMP_VELOCITY : float = 4.5
+const ACCELERATION : float  = 10
+# Private Variables
+var _input_direction : Vector2  = Vector2.ZERO
+var _current_direction : Vector3 = Vector3.ZERO
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -11,16 +14,17 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("action") and is_on_floor():
+		#case if nearby object else jump
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+	_input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	_current_direction = (transform.basis * Vector3(_input_direction.x, 0,_input_direction.y)).normalized()
+	if _current_direction:
+		velocity.x = move_toward(velocity.x, _current_direction.x * SPEED, ACCELERATION)
+		velocity.z =move_toward(velocity.z, _current_direction.z * SPEED, ACCELERATION)
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
