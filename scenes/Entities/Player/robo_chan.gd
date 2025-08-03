7993eec8d165fb7676fdb04b7b4e4dfaf0309c3a
Yannik Braendle <yannik.braendle@t-online.de>
2025-08-03 17:20:20 +0200

    interaction behaviour overhaul

--- scenes/Entities/Player/robo_chan.gd
+++ scenes/Entities/Player/robo_chan.gd
@@ -2,7 +2,7 @@ extends CharacterBody3D
 @onready var player_visual_node : Node3D = $robo_chan_model
 @onready var _particle_system : CPUParticles3D = $CPUParticles3D
 @onready var interacting_fake : Node3D = $alien_crate
-const SPEED : float = 4.5
+const SPEED : float = 7.5
 const JUMP_VELOCITY : float = 2.5
 const ACCELERATION : float  = 8
 const LOOK_SMOOTHNESS : float = 8.0
@@ -25,8 +25,9 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	_input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	
 	# calculate input direction from Characterbody3D
-	_next_direction = (transform.basis * Vector3(_input_direction.x, 0,_input_direction.y)).normalized()
+	_next_direction = (transform.basis * Vector3(_input_direction.x, 0,_input_direction.y)).normalized().rotated(Vector3.UP, -rotation.y)
 	# if any input
 	if _next_direction:
 		velocity.x = move_toward(velocity.x, _next_direction.x * SPEED, ACCELERATION)
@@ -36,8 +37,10 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 	#apply visual to look to direction
-	if _next_direction.length() > 0.1:
-		player_visual_node.look_at(player_visual_node.global_position + Vector3(_input_direction.x,0,_input_direction.y), Vector3(0, 1, 0))
+	# if _next_direction.length() > 0.1:
+	# 	player_visual_node.look_at(player_visual_node.global_position + Vector3(_input_direction.x,0,_input_direction.y), Vector3(0, 1, 0))
+	if not _input_direction.is_zero_approx():
+		look_at(player_visual_node.global_position + Vector3(_input_direction.x,0,_input_direction.y), Vector3(0, 1, 0))
 	#apply velocity and slide
 	move_and_slide()
 
--- scenes/UI/Interacting_manager/interacting_manger.gd
+++ scenes/UI/Interacting_manager/interacting_manger.gd
@@ -1,13 +1,13 @@
 extends Node3D
 class_name Interacting_Manager
 
-@onready var _interact_sprite : Node3D = $ui_interact
+@onready var _interact_sprite: Node3D = $ui_interact
 
 #Private Variables
-var _caller : Node3D  = null
+var _caller: Node3D = null
 # holding interactable
-var _current_holding_object : Node3D = null
-var _player : Node3D = null
+var _current_holding_object: Node3D = null
+var _player: Node3D = null
 
 # set player
 func _ready() -> void:
@@ -24,51 +24,78 @@ func _unhandled_input(event):
 			_drop_interactable()
 
 # set _calller and show interactable sprite
-func start_interacting_on_position(_pos : Vector3, caller : Node3D) ->void:
+func start_interacting_on_position(_pos: Vector3, caller: Node3D) -> void:
 	_interact_sprite.global_position = _pos
 	_interact_sprite.show()
 
 	_caller = caller
 
 # hide interactable sprite
-func stop_interacting_on_position() ->void:
+func stop_interacting_on_position() -> void:
 	_interact_sprite.hide()
 	_caller = null
 
-func _on_input_cast_interact() ->void:
+func _on_input_cast_interact() -> void:
 	_caller.interact()
 	_interact_sprite.hide()
 	_caller = null
 ## Check if holding and set new current interactable and apply it to hand left
-func set_new_interactable(_node : Node3D) ->void:
+func set_new_interactable(_node: Node3D) -> void:
 	if _current_holding_object != null:
 		## drop current holding interactable
 		_drop_interactable()
 	if _node:
-
 		# apply new current holding interactable item
 		_current_holding_object = _node
+		_node.get_parent().remove_child(_node)
+		_player.add_child(_node)
+		var new_pos = _player.global_position + -_player.global_transform.basis.z * 3.0 + Vector3.UP
+		if _node is RigidBody3D:
+			for child in _node.get_children():
+				if child is CollisionShape3D:
+					child.set_deferred("disabled", true)
+			PhysicsServer3D.body_set_state(
+				_node.get_rid(),
+				PhysicsServer3D.BODY_STATE_TRANSFORM,
+				Transform3D.IDENTITY.translated(Vector3.ZERO),
+			)
+			_node.position = Vector3.ZERO
+			_node.global_position = new_pos
+			_node.freeze = true
+			_node.linear_velocity = Vector3.ZERO
+		else:
+			_node.global_position = new_pos
 		#remove mother and add as child to hand left
-		_current_holding_object.hide()
-		_current_holding_object.set_deferred("freeze", true)
+		# _current_holding_object.hide()
+		# _current_holding_object.set_deferred("freeze", true)
 		#_interacting_fake
-		get_tree().get_first_node_in_group("player").interacting_fake.show()
-	else: 
+		# get_tree().get_first_node_in_group("player").interacting_fake.show()
+	else:
 		print("Interacting_Manager Error:  Null Object _node -> on set_new_interactable")
 
 ## drop current holding interactable
-func _drop_interactable() ->void:
+func _drop_interactable() -> void:
 	print(_current_holding_object)
 	if _current_holding_object:
+		var temp_pos = _current_holding_object.global_position
 		#save position
-		get_tree().get_first_node_in_group("player").interacting_fake.hide()
-		
-		var _temp_pos :Vector3 = get_tree().get_first_node_in_group("player").interacting_fake.global_position
+		if _current_holding_object is RigidBody3D:
+			_current_holding_object.freeze = false
+			_player.remove_child(_current_holding_object)
+			_player.get_parent().add_child(_current_holding_object)
+			for child in _current_holding_object.get_children():
+				if child is CollisionShape3D:
+					child.set_deferred("disabled", false)
+		else:
+			_player.remove_child(_current_holding_object)
+			_player.get_parent().add_child(_current_holding_object)
+		# get_tree().get_first_node_in_group("player").interacting_fake.hide()
+		_current_holding_object.global_position = temp_pos
+		# var _temp_pos: Vector3 = get_tree().get_first_node_in_group("player").interacting_fake.global_position
 		# remove holding interactable from hand
-		_current_holding_object.drop_effect(_temp_pos)
-		_current_holding_object.show()
+		# _current_holding_object.drop_effect(_temp_pos)
+		# _current_holding_object.show()
+		_current_holding_object._is_holding = false
 		_current_holding_object = null
-	else: 
+	else:
 		print("Interacting_Manager Error:  Null Object on _drop_interactable")
-
-
